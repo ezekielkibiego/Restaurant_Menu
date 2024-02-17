f23326cfaecb7aa24d13c226f8da5c1c6ee7557f
ezekielkibiego <kibiezekiel@gmail.com>
2024-02-17 13:07:24 +0300

    add script.js file

---
+++ script.js
@@ -0,0 +1,8 @@
+let message = "Hello Zindua!";
+let num1 = 3;
+let num2 = 2;
+let shopping = ['bread', 'sugar', 'milk'];
+let solution = num1 * num2;
+
+console.log(solution);
+console.log(shopping)
