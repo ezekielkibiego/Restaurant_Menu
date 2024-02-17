55fef5b69208574628be06f22b092567de6e8485
ezekielkibiego <kibiezekiel@gmail.com>
2024-02-17 13:17:57 +0300

    add concatination logic

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let message = "Hello Zindua!";
+let message = "Hello Zindua,";
 let num1 = 3;
 let num2 = 2;
 let shopping = ['bread', 'sugar', 'milk'];
 let solution = num1 * num2;
+let name = "Kib";
 
+let newMessage = `${message} my name is ${name}.`;
 console.log(solution);
 console.log(shopping)
+console.log(newMessage);
